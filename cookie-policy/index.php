100b31e3c0bb5964e237ab9ece628237963c8f06
Wing Him Liu <winghim_01@hotmail.com>
2017-01-26 02:06:42 +0000

    FEATURE : google analytics is added to the home page and cookie policy page.

--- cookie-policy/index.php
+++ cookie-policy/index.php
@@ -38,5 +38,16 @@
 		<!-- bootstrap css -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha256-916EbMg70RQy9LHiGkXzG8hSg9EdNy97GazNG/aiY1w=" crossorigin="anonymous" />
 		
+		<!-- google analytics -->
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-90935716-1', 'auto');
+			ga('send', 'pageview');
+		</script>
+		
 	</body>
 </html>
